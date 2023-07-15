bfb58a219c3f59e25ed6ce4771e92a06843be417
Kamil Kawecki <kamil.kawecki@gmail.com>
2023-07-15 04:14:28 +0200

    fix lint, add images fallback

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -2,7 +2,7 @@ function ListItem(props) {
   return (
     <li className="bg-white rounded overflow-hidden">
       <div>
-        <img src="/LukeSkywalker.jpg" alt="" width="300" height="300" />
+        <img src={'/LukeSkywalker.jpg'} onError={(e)=>{e.target.onError = null; e.target.src = '/placeholder.png'}} alt="" width="300" height="300" />
       </div>
       <div className="p-4 text-center">{props.person.name}</div>
     </li>
